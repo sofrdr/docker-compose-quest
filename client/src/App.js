45931a1c96376a39c5d74d5d4c0fb4215fa09d75
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-04-14 14:55:15 +0200

    Change port to fix hot reload

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test App 
+          Test App Docker
         </p>
         <a
           className="App-link"
